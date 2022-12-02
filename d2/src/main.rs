98ff4f882cd8dcb4a34b1bf6635653ecdb63081a
Your Name <you@example.com>
2022-12-02 08:59:12 +0000

    add part 1 /2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -1,14 +1,50 @@
 fn part1(){
     let input = include_str!("day2.txt");
     let mut myscore = 0;
+    let mut enemy =0;
     // A / X = rock     1
     // B / Y = Paper    2 
     // C / Z = Scissors 3
     // win +6
     // Draw +3
-    // X lost
-    // Y draw
-    // Z win
+    for row in input.lines(){
+	let rowch = row.chars().collect::<Vec<char>>();
+	match (rowch[0], rowch[2]) {
+	    ('A','X') => {
+		myscore += 4;
+		enemy += 4;
+	    },
+	    ('A','Y') => {myscore+=8;
+			    enemy+=1;},
+	    ('A','Z') => {myscore+=3;
+			    enemy+=7;},
+	    ('B','X') => {myscore+=1;
+			    enemy+=8;},
+	    ('B','Y') => {myscore+=5;
+			    enemy+=5;},
+	    ('B','Z') => {myscore+=9;
+			    enemy+=2;},
+	    ('C','X') => {myscore+=7;
+			    enemy+=3;},
+	    ('C','Y') => {myscore+=2;
+			    enemy+=9;},
+	    ('C','Z') => {myscore+=6;
+			  enemy+=6;},
+	    (_,_) => {},
+	}
+    }
+    println!("my score {}", myscore);
+}
+
+fn part2(){
+    let input = include_str!("day2.txt");
+    let mut myscore = 0;
+    let mut enemy =0;
+    // A / X = rock     1
+    // B / Y = Paper    2 
+    // C / Z = Scissors 3
+    // win +6
+    // Draw +3
     for row in input.lines(){
 	let rowch = row.chars().collect::<Vec<char>>();
 	match (rowch[0], rowch[2]) {
@@ -26,9 +62,8 @@ fn part1(){
     }
     println!("my score {}", myscore);
 }
-
 fn main() {
     part1();
-
+    part2();
     
 }
